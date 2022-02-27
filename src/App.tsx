6697aa090415dac62274cd996ca9329b3c04c4dd
lpk <734058297@qq.com>
2022-02-28 00:08:50 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import './App.scss';
+import axios from 'axios';
 
 function App() {
+  useEffect(()=>{
+    axios.get('/ajax/moreComingList?token=&movieIds=1287590,1445469,1277950,1277951,1357865,1354748,1328669,1337941,345276,1359434&optimus_uuid=7927D76097E211ECB3CE3BDBF8552D9DB1227BE564804EB78671CE7DF1E1642C&optimus_risk_level=71&optimus_code=10').then(res=>{
+      console.log(res,'res')
+    })
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      app
+      <div>12</div>
     </div>
   );
 }
