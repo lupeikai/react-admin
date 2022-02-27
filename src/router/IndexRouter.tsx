7c64e534bf4a1cbd6651f11f2c9e4af36f3caf32
lpk <734058297@qq.com>
2022-02-28 00:48:07 +0800

    router

---
+++ src/router/IndexRouter.tsx
@@ -0,0 +1,14 @@
+import React,{FC} from 'react'
+import {HashRouter as Router,Route,Switch} from 'react-router-dom';
+import Login from '../views/Login';
+import NewsSanBox from '../views/sanBox/NewsSanBox';
+export default function IndexRouter() {
+  return (
+    <Router>
+      <Switch>
+        <Route path='/login' component={Login}/>
+        <Route path='/NewsSanBox' component={NewsSanBox}/>
+        </Switch>
+    </Router>
+  )
+}
